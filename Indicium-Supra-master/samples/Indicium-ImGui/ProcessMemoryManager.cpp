039784b2217fd3bc7c75e963060e2ee7e0bc69d0
Tom Solacroup <tom.solacroup@gmail.com>
2018-06-01 13:38:43 +0200

    processmemorymanager

--- Indicium-Supra-master/samples/Indicium-ImGui/ProcessMemoryManager.cpp
+++ Indicium-Supra-master/samples/Indicium-ImGui/ProcessMemoryManager.cpp
@@ -1,12 +1,24 @@
 #include "ProcessMemoryManager.h"
+#include <windows.h>
+#include <cstdio>
+#include <tlhelp32.h>
 
-
-
-ProcessMemoryManager::ProcessMemoryManager()
+unsigned char * ProcessMemoryManager::ReadMemory(int dataOffset, int length, bool offsetIsPointer, int additionalOffset)
 {
+	HMODULE hmod = GetModuleHandle(0);
+	SIZE_T bytesRead = 0;
+	unsigned char* data = new unsigned char[length];
+	int offset = offsetIsPointer ? GetOffsetFromPointer(dataOffset) : dataOffset;
+	ReadProcessMemory(hmod, (LPCVOID)(offset + additionalOffset), data, length, &bytesRead);
+	return data;
 }
 
-
-ProcessMemoryManager::~ProcessMemoryManager()
+void ProcessMemoryManager::WriteProcessMemory(int dataOffset, unsigned char * data, bool offsetIsPointer, int additionalOffset)
 {
+	//TODO
+}
+
+int ProcessMemoryManager::GetOffsetFromPointer(int pointerOffset)
+{
+	return 0;
 }
--- Indicium-Supra-master/samples/Indicium-ImGui/ProcessMemoryManager.h
+++ Indicium-Supra-master/samples/Indicium-ImGui/ProcessMemoryManager.h
@@ -1,8 +1,14 @@
 #pragma once
-class ProcessMemoryManager
+#include <string>
+static class ProcessMemoryManager
 {
 public:
-	ProcessMemoryManager();
-	~ProcessMemoryManager();
+	static unsigned char* ReadMemory(int dataOffset, int length, bool offsetIsPointer, int additionalOffset = 0);
+	static void WriteProcessMemory(int dataOffset, unsigned char* data, bool offsetIsPointer, int additionalOffset = 0);
+	static int GetOffsetFromPointer(int pointerOffset);
+	
+private:
+	const int _PROCESS_WM_READ = 0x0010;
+	const int _PROCESS_VM_WRITE = 0x0020;
+	const int _PROCESS_VM_OPERATION = 0x0008;
 };
-
